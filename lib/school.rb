a3fef1c0a6e21a5469d600799c030bb42a228803
Marc Mills <mdmills1990@gmail.com>
2018-08-29 13:41:19 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -2,6 +2,7 @@ class School
 
   def initialize(name)
     @name = name
+    @roster = {}
   end
 
   attr_accessor :roster
@@ -9,8 +10,6 @@ class School
 
 def add_student(grade, student)
   @roster[grade] = []
-  #some if statement that suggests if the key is already in the
-  #hash, push the key's value into an array with the other value.
   @roster[grade] << student
 end
 
@@ -22,3 +21,7 @@ def sort
 end
 
 end
+
+
+#some if statement that suggests if the key is already in the
+#hash, push the key's value into an array with the other value.
