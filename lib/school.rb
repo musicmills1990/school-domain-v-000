74d1b7413d28a198d26ce60192d9ab9964306826
Marc Mills <mdmills1990@gmail.com>
2018-08-29 12:52:06 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,23 @@
-# code here!
+class School
+
+  def initialize(name)
+    @name = name
+  end
+
+def roster
+{}
+end
+
+def add_student(grade, student)
+  roster[grade] = []
+  roster[grade] << ["#{student}"]
+end
+
+def grade(grade)
+
+end
+
+def sort
+end
+
+end
